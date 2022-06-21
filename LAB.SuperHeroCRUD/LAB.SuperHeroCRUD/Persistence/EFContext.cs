8f0fb4d130f366a08cd6c92b8f92552b19894dda
FabricioOrtiz <fabricio.ortiz.alfaro@gmail.com>
2022-06-20 18:34:47 -0600

    adding contract to efcontext

--- LAB.SuperHeroCRUD/LAB.SuperHeroCRUD/Persistence/EFContext.cs
+++ LAB.SuperHeroCRUD/LAB.SuperHeroCRUD/Persistence/EFContext.cs
@@ -1,9 +1,11 @@
 ﻿using LAB.SuperHeroCRUD.Model;
+using LAB.SuperHeroCRUD.Persistence.Contract;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace LAB.SuperHeroCRUD.Persistence
 {
-    public class EFContext : DbContext
+    public class EFContext : DbContext, IApplicationDbContext
     {
         public EFContext(DbContextOptions<EFContext> options) : base(options)
         {
@@ -12,5 +14,6 @@ namespace LAB.SuperHeroCRUD.Persistence
 
         public DbSet<SuperHero> SuperHero { get; set; }
 
+        public IDbConnection Connection => Database.GetDbConnection();
     }
 }
